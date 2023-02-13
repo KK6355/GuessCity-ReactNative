e45963debb0e40e26c045069191600d827a28d2f
Vicky_Kang <93057655+KK6355@users.noreply.github.com>
2023-02-13 14:56:39 +1300

    Create tab navigation

---
+++ src/components/DbList.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function DbList() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Database!</Text>
+    </View>
+  );
+}
+
+export default DbList;
---
+++ src/components/Game.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function Game() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Game!</Text>
+    </View>
+  );
+}
+
+export default Game;
