dabed4be2bfcd456f13d1334dd5dc98340ba4d07
Vicky_Kang <93057655+KK6355@users.noreply.github.com>
2023-02-16 16:43:34 +1300

    added database page

--- src/components/DbList.js
+++ src/components/DbList.js
@@ -1,11 +1,108 @@
-import { View, Text } from "react-native";
+import * as SQLite from "expo-sqlite";
+import { useEffect, useState } from "react";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 
-function DbList() {
+const DbList = () => {
+  const db = SQLite.openDatabase("guessCityDB.db");
+  const [isLoading, setIsLoading] = useState(false);
+  const [cities, setCities] = useState([]);
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS cities (id INTEGER PRIMARY KEY AUTOINCREMENT, city TEXT)",
+        console.log("created table")
+      );
+    });
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM cities",
+        null,
+        (txObj, resultSet) => setCities(resultSet.rows._array),
+        console.log("fetch table")
+      ),
+        (txObj, error) => console.error(error);
+    });
+    setIsLoading(false);
+  }, []);
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  const getCity = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS cities (id INTEGER PRIMARY KEY AUTOINCREMENT, city TEXT)",
+        console.log("created table")
+      );
+    });
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM cities",
+        null,
+        (txObj, resultSet) => setCities(resultSet.rows._array),
+        console.log("fetch table")
+      ),
+        (txObj, error) => console.error(error);
+    });
+  };
+  const deleteAll = () => {
+    db.transaction((tx) => {
+      tx.executeSql("DELETE * FROM cities", console.log("delete all data")),
+        (txObj, error) => console.error(error);
+    });
+  };
+  const deleteCity = (id) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "DELETE FROM cities WHERE id=?",
+        [id],
+        (txObj, resultSet) => {
+          if (resultSet.rowsAffected > 0) {
+            let existingCities = [...cities].filter((city) => city.id !== id);
+            setCities(existingCities);
+          }
+        },
+        console.log("deleted data"),
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const showCities = () => {
+    return cities.map((city, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text style={{ color: "#fff" }}>{city.city}</Text>
+          <Button title="Delete" onPress={() => deleteCity(city.id)} />
+        </View>
+      );
+    });
+  };
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Database!</Text>
+    <View style={styles.container}>
+      <Button title="Refresh Cities Data" onPress={getCity} />
+      <Button title="Delete All Cities" onPress={deleteAll} />
+      {showCities()}
     </View>
   );
-}
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#023047",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
+    margin: 8,
+  },
+});
 
 export default DbList;
