d6ad82b00d9303129e7073c49cc50f16cf52f6f6
Vicky_Kang <93057655+KK6355@users.noreply.github.com>
2023-02-16 18:28:59 +1300

    add tab nav icons

--- App.js
+++ App.js
@@ -5,13 +5,38 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Game from "./src/components/Game";
 import Api from "./src/components/Api";
 import DbList from "./src/components/DbList";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faCirclePlay,
+  faDatabase,
+  faCloudArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Game") {
+              iconName = faCirclePlay;
+            } else if (route.name === "API") {
+              iconName = faCloudArrowUp;
+            } else {
+              iconName = faDatabase;
+            }
+            return (
+              <FontAwesomeIcon icon={iconName} size={size} color={color} />
+            );
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen name="Game" component={Game} />
         <Tab.Screen name="API" component={Api} />
         <Tab.Screen name="Database" component={DbList} />
